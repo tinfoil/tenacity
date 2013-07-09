d639f213e0a527dfb27a6b3fc91f6dba44f6d183
John Gerhardt <john@market76.com>
2013-07-09 12:38:50 -0400

    Test dependent destroy on polymorphic objects

--- spec/tenacity_spec.rb
+++ spec/tenacity_spec.rb
@@ -50,6 +50,12 @@ describe Tenacity do
       firm.comments.count.should > 0
     end
 
+    it 'destroys dependent objects' do
+      document_id = document_with_comments.id.to_s
+      document_with_comments.destroy
+      Comment.where(commentable_id: document_id, commentable_type: 'Document').count.should be(0)
+    end
+
     it 'returns proper belongs_to results' do
       document_with_comments.firm.id.should == firm.id
     end
